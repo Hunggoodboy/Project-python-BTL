d7b1fc69c7d2417ff01e14be0551f2c03862b4e3
Hunggoodboy <Hungmk2005@gmail.com>
2025-10-16 00:01:03 +0700

    Hung

---
+++ database/connect.py
@@ -0,0 +1,9 @@
+import mysql.connector
+import fcntl
+
+connection = mysql.connector.connect(
+    host = "127.0.0.1",
+    user = "root",
+    passwd = "hung0335321015",
+    database = "QLBanQuanAo"
+)
---
+++ database/database.sql
@@ -0,0 +1,44 @@
+CREATE database IF NOT EXISTS QLBanQuanAo;
+
+USE QLBanQuanAo;
+
+CREATE TABLE IF NOT exists KhachHang(
+	MaKH int primary key,
+    HoTen nvarchar(100) NOT NULL,
+    Email varchar(100) NOT NULL,
+    MatKhau varchar(100) NOT NULL,
+    SDT varchar(10) NOT NULL,
+    Address nvarchar(100)
+);
+
+CREATE TABLE IF NOT EXISTS DanhMuc(
+	MaDM int Primary KEY,
+    TenDM nvarchar(100) NOT NULL,
+    MoTa nvarchar(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SanPham(
+	MaSP int primary key,
+    MaDM int,
+    foreign key(MaDM) references DanhMuc(MaDM),
+    TenSP nvarchar(100),
+    MoTa nvarchar(100),
+    Gia float,
+    MauSac nvarchar(100),
+    Size nvarchar(100),
+    ChatLieu nvarchar(100),
+    SoLuongcon int,
+    HinhAnh LONGBLOB
+);
+
+CREATE TABLE IF NOT EXISTS DonHang(
+	MaDH int primary key,
+    MaSP int,
+    MaKH int,
+    foreign key(MaSP) references SanPham(MaSP),
+    foreign key(MaKH) references KhachHang(MaKH),
+    SoLuong int,
+    DonGia int,
+    TongGia int
+);
+
