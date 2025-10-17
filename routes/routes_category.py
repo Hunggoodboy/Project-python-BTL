459819a19d1c87cd3134de8fb0eac462061940d7
dhm-005 <daohamyvn2005@gmail.com>
2025-10-18 00:21:01 +0700

    edit vài file

--- routes/routes_category.py
+++ routes/routes_category.py
@@ -16,5 +16,16 @@ def category(category):
     conn.close()
     return render_template(f"{category}.html", products=products)
 
+@category_bp.route('/collections/<season>')
+def collection(season):
+    conn = get_connect()
+    cursor = conn.cursor(dictionary=True)
+    sql = "SELECT * FROM QLBanQuanAo.SanPham WHERE MoTa = %s"
+    cursor.execute(sql, [f"%{season}%"])
+    products = cursor.fetchall()  # tất cả sản phẩm
+    cursor.close()
+    conn.close()
+    return render_template("collections.html", products=products)
+
 
 
