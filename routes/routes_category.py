43f7b6981e639fd9b80d0bdc725c9643e4f51791
Hunggoodboy <Hungmk2005@gmail.com>
2025-10-17 14:28:16 +0700

    sửa lại

--- routes/routes_category.py
+++ routes/routes_category.py
@@ -14,25 +14,7 @@ def category(category):
     products = cursor.fetchall()  # tất cả sản phẩm
     cursor.close()
     conn.close()
-    return render_template("Ao.html", products=products)
-def category(category):
-    conn = get_connect()
-    cursor = conn.cursor(dictionary=True)
-    sql = "SELECT * FROM QLBanQuanAo.SanPham WHERE MoTa = %s"
-    cursor.execute(sql, [f"%{category}%"])
-    products = cursor.fetchall()  # tất cả sản phẩm
-    cursor.close()
-    conn.close()
-    return render_template("Quan.html", products=products)
-def category(category):
-    conn = get_connect()
-    cursor = conn.cursor(dictionary=True)
-    sql = "SELECT * FROM QLBanQuanAo.SanPham WHERE MoTa = %s"
-    cursor.execute(sql, [f"%{category}%"])
-    products = cursor.fetchall()  # tất cả sản phẩm
-    cursor.close()
-    conn.close()
-    return render_template("PhuKien.html", products=products)
+    return render_template(f"{category}.html", products=products)
 
 
 
