80dbe28f26b7c5069a03dbda163b2a555b2d7e9f
nguyenquehoang012005-a1ly <nguyenquehoang18012005@gmail.com>
2025-10-21 10:25:19 +0700

    Them, sua, xoa san pham/ quan ly nguoi dung, san pham app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import base64
 from database.connect import get_connect
 import flask
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 #from DB_connect import get_db_connection
 import os
 from database.connect import get_connect
@@ -54,6 +54,204 @@ def show_user():
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route('/product')
+def show_product():
+    conn = get_connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM qlbanquanao.sanpham")
+    products = cursor.fetchall()
+    cursor.close()
+    print("tat ca san pham", products)
+    return render_template('ProductTable.html', products=products)
+
+@app.route('/product/add', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        MaSP = request.form['MaSP']
+        MaDM = request.form['MaDM']
+        TenSP = request.form['TenSP']
+        MoTa = request.form['MoTa']
+        Gia = request.form['Gia']
+        MauSac = request.form['MauSac']
+        Size = request.form['Size']
+        ChatLieu = request.form['ChatLieu']
+        SoLuongCon = request.form['SoLuongCon']
+        HinhAnh = request.form['HinhAnh']
+        Season = request.form['Season']
+
+        conn = get_connect()
+        cursor = conn.cursor()
+        cursor.execute("""
+                       INSERT INTO QLBanQuanAo.SanPham
+                       (MaSP, MaDM, TenSP, MoTa, Gia, MauSac, Size, ChatLieu, SoLuongCon, HinhAnh, Season)
+                       VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                       """,
+                       (MaSP, MaDM, TenSP, MoTa, Gia, MauSac, Size, ChatLieu, SoLuongCon, HinhAnh, Season))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('show_product'))
+    return render_template('newProduct.html')
+
+@app.route('/product/delete/<string:MaSP>', methods=['POST'])
+def delete_product(MaSP):
+    if 'user_role' not in session or session['user_role'] != 'admin':
+        flash('Bạn không có quyền thực hiện hành động này!')
+        return redirect(url_for('menu.main_menu'))
+    if request.method == 'POST':
+        try:
+            conn = get_connect()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM QLBanQuanAo.SanPham WHERE MaSP = %s" , (MaSP,))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            # (Tùy chọn) Gửi thông báo thành công
+            flash('Đã xóa sản phẩm thành công!')
+
+        except Exception as e:
+            print(f"Lỗi khi xóa sản phẩm: {e}")
+            # (Tùy chọn) Gửi thông báo lỗi
+            flash('Xóa sản phẩm thất bại, đã có lỗi xảy ra.')
+        return redirect(url_for('show_product'))
+
+@app.route('/product/edit/<string:MaSP>', methods=['GET', 'POST'])
+def edit_product(MaSP):
+    if 'user_role' not in session or session['user_role'] != 'admin':
+        flash('Bạn không có quyền thực hiện hành động này!')
+        return redirect(url_for('menu.main_menu'))
+    if request.method == 'POST':
+        try:
+            MaDM = request.form['MaDM']
+            TenSP = request.form['TenSP']
+            MoTa = request.form['MoTa']
+            Gia = request.form['Gia']
+            MauSac = request.form['MauSac']
+            Size = request.form['Size']
+            ChatLieu = request.form['ChatLieu']
+            SoLuongCon = request.form['SoLuongCon']
+            HinhAnh = request.form['HinhAnh']
+            Season = request.form['Season']
+
+            conn = get_connect()
+            cursor = conn.cursor()
+            cursor.execute("""UPDATE QLBanQuanAo.SanPham 
+                  SET MaDM = %s, 
+                      TenSP = %s, 
+                      MoTa = %s, 
+                      Gia = %s, 
+                      MauSac = %s, 
+                      Size = %s, 
+                      ChatLieu = %s, 
+                      SoLuongCon = %s, 
+                      HinhAnh = %s, 
+                      Season = %s
+                  WHERE MaSP = %s""",
+               (MaDM, TenSP, MoTa, Gia, MauSac, Size, ChatLieu, SoLuongCon, HinhAnh, Season, MaSP))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return redirect(url_for('show_product'))
+
+        except Exception as e:
+            print(f"Lỗi khi cập nhật sản phẩm: {e}")
+            # (Tùy chọn) Gửi thông báo lỗi
+            flash('Cập nhật thất bại, có lỗi xảy ra.')
+            return redirect(url_for('show_product'))
+    else:  # Tức là request.method == 'GET'
+        try:
+            conn = get_connect()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM QLBanQuanAo.SanPham WHERE MaSP = %s", (MaSP,))
+            product = cursor.fetchone()
+
+            cursor.close()
+            conn.close()
+
+            if product:
+                # Gửi dữ liệu sản phẩm này sang file
+                return render_template('editProduct.html', product=product)
+            else:
+                # Nếu không tìm thấy sản phẩm
+                return "Không tìm thấy sản phẩm."
+        except Exception as e:
+            print(f"Lỗi khi lấy thông tin sản phẩm: {e}")
+            return "Đã xảy ra lỗi."
+
+
+@app.route('/user/edit/<string:MaKH>', methods=['GET', 'POST'])
+def edit_user(MaKH):
+    # Kiểm tra quyền admin
+    if 'user_role' not in session or session['user_role'] != 'admin':
+        flash('Bạn không có quyền thực hiện hành động này!')
+        return redirect(url_for('menu.main_menu'))
+
+    conn = get_connect()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        try:
+            HoTen = request.form['HoTen']
+            UserName = request.form['UserName']
+            MatKhau = request.form['MatKhau']
+            SDT= request.form['SDT']
+            Address = request.form['Address']
+
+
+            cursor.execute("""
+                           UPDATE QLBanQuanAo.KhachHang
+                           SET HoTen = %s,
+                               UserName = %s,
+                               MatKhau = %s,
+                               SDT = %s,
+                               Address = %s,
+                           WHERE MaKH = %s
+                           """, (HoTen, UserName, MatKhau, SDT, MaKH))
+            conn.commit()
+            flash('Cập nhật thông tin người dùng thành công!')
+        except Exception as e:
+            print(f"Lỗi khi cập nhật người dùng: {e}")
+            flash('Cập nhật thất bại, có lỗi xảy ra.')
+            cursor.close()
+            conn.close()
+        return redirect(url_for('show_user'))
+
+    else:  # (request.method == 'GET')
+        try:
+
+            cursor.execute("SELECT * FROM QLBanQuanAo.KhachHang WHERE MaKH = %s", (MaKH,))
+            user = cursor.fetchone()
+            cursor.close()
+            conn.close()
+            if user:
+                return render_template('editUser.html', user=user)
+            else:
+                return "Không tìm thấy người dùng."
+        except Exception as e:
+            print(f"Lỗi khi lấy thông tin người dùng: {e}")
+            return "Đã xảy ra lỗi."
+
+
+@app.route('/user/delete/<string:MaKH>', methods=['POST'])
+def delete_user(MaKH):
+    # Kiểm tra quyền admin
+    if 'user_role' not in session or session['user_role'] != 'admin':
+        flash('Bạn không có quyền thực hiện hành động này!')
+        return redirect(url_for('menu.main_menu'))
+
+    if request.method == 'POST':
+        try:
+            conn = get_connect()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM QLBanQuanAo.KhachHang WHERE MaKH = %s", (MaKH,))
+            conn.commit()
+            cursor.close()
+            conn.close()
+            flash('Đã xóa người dùng thành công!')
+        except Exception as e:
+            print(f"Lỗi khi xóa người dùng: {e}")
+            flash('Xóa người dùng thất bại.')
+        return redirect(url_for('show_user'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
