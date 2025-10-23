367918f233e90d938718f9cd9765c34e41635824
dhm-005 <daohamyvn2005@gmail.com>
2025-10-23 23:16:58 +0700

    push lại filter

--- routes/routes_main.py
+++ routes/routes_main.py
@@ -14,36 +14,3 @@ def main_menu():
     cursor.close()
     conn.close()
     return render_template('mainMenu.html',products=products )
-
-@main_bp.route('/search')
-def search():
-    word = request.args.get('word', '')   # lấy từ input
-    conn = get_connect()
-    cursor = conn.cursor(dictionary=True)
-    conn = get_connect()
-    cursor = conn.cursor(dictionary=True)
-    sql = ("""
-            SELECT *
-            FROM QLBanQuanAo.SanPham 
-            WHERE ChatLieu LIKE %s 
-                OR TenSP LIKE %s
-                OR MauSac LIKE %s
-                or MoTa LIKE %s
-            ORDER BY
-                CASE
-                    WHEN TenSP LIKE %s THEN 1
-                    WHEN ChatLieu LIKE %s THEN 2
-                    ELSE 3
-                END,
-                TenSP;
-            """)
-    key = f"%{word}%"
-    print(key)
-    cursor.execute(sql, [key, key, key, key, key, key])
-    products = cursor.fetchall()  # tất cả sản phẩm
-    cursor.close()
-    conn.close()
-    if products:
-        return render_template("search.html", word=word, products=products)
-    else:
-        return render_template("search.html", word=word, products='')
