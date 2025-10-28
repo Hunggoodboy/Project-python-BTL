9adba0a320d6247125458098c77f95738c2a1d9d
Huy <loll73382@gmail.com>
2025-10-28 20:25:41 +0700

    app.py(bp)

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from routes.routes_delete_user import delete_userbp
 from routes.routes_delete_product import delete_productbp
 from routes.routes_show_product import show_productbp
 from routes.routes_orders import orders_bp
+from routes.routes_cart import cart_bp
 app = Flask(__name__)
 app.secret_key = '123456'
 
@@ -48,9 +49,10 @@ app.register_blueprint(edit_productbp)
 app.register_blueprint(addproduct_bp)
 app.register_blueprint(delete_productbp)
 app.register_blueprint(delete_userbp)
-
 app.register_blueprint(show_productbp)
 app.register_blueprint(orders_bp)
+app.register_blueprint(cart_bp)
+
 @app.route('/new-product')
 def new_product():
     return render_template('newProduct.html')
