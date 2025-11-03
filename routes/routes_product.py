341eb49b37348164eeb933114bfa0b980078d501
dhm-005 <daohamyvn2005@gmail.com>
2025-11-03 16:05:07 +0700

    fix link ảnh

--- routes/routes_product.py
+++ routes/routes_product.py
@@ -21,12 +21,31 @@ def product_detail(pid):
         # Tính giá sau giảm
         product['GiaSauGiam'] = int(product['Gia'] * (100 - product['Discount']) / 100)
 
-        product['images'] = [product['HinhAnh']]
+        # tách ảnh phụ
+        path = product['HinhAnh'] #vd: anh_quan_ao/ao/ao_thun_basic.png
+        if '/ao/' in path:
+            subdir = 'anh_phu_ao'
+        elif '/quan/' in path:
+            subdir = 'anh_phu_quan'
+        else:
+            subdir = 'anh_phu_phu_kien'
+
+        sep = path.split('/')
+        newpath = '/'.join([sep[0], subdir, sep[-1]]) #vd: anh_quan_ao/anh_phu_ao/ao_thun_basic.png
+        colors = [c.strip() for c in product['MauSac'].split(',')]
+        product['colors'] = [
+            {
+                'name': color,
+                'image': f"{newpath.rsplit('.', 1)[0]}_{i + 1}.jpg"
+            }
+            for i, color in enumerate(colors)
+        ]
 
         print("\n==== PRODUCT DEBUG ====")
         print(product)
         print("product['MaSP'] =", product.get('MaSP'))
         print("product['id'] =", product.get('MaSP'))
+        print("product['colors'] =", product.get('colors'))
         print("========================\n")
 
         return render_template('productDetail.html', product=product)
