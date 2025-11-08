a21ab75da3962ba8bd01bd6d61a14e09d5ac2254
Hunggoodboy <Hungmk2005@gmail.com>
2025-11-08 15:41:46 +0700

    thêm 4 trạng thái

--- routes/routes_quanlydonhang.py
+++ routes/routes_quanlydonhang.py
@@ -4,18 +4,69 @@ from flask import Blueprint, render_template, request
 
 qldonhangbp = Blueprint('qldonhangbp', __name__)
 
-qldonhangbp.route("/quanlydonhang", methods=["POST"])
-def quanlydonhang():
+qldonhangbp.route("/quanlydonhang_Choxacnhan", methods=["POST"])
+def quanlydonhang_Choxacnhan():
     conn = get_connect()
     cursor = conn.cursor()
-    sql = "select * from QLBanQuanAo.DonHang"
+    sql = """
+    SELECT *
+    FROM QLBanQuanAo.DonHang
+    Where TrangThai Like 'Chờ xác nhận'
+    """
     cursor.execute(sql)
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
     return render_template('adminorder.html', rows=rows)
 
-qldonhangbp.route("/setTrangThai", methods=["POST"])
+
+qldonhangbp.route("/quanlydonhang_Danggiaohang", methods=["POST"])
+def quanlydonhang_Danggiaohang():
+    conn = get_connect()
+    cursor = conn.cursor()
+    sql = """
+    SELECT *
+    FROM QLBanQuanAo.DonHang
+    Where TrangThai Like 'Đang giao hàng'
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('adminorder.html', rows=rows)
+
+qldonhangbp.route("/quanlydonhang_Chobanxacnhan", methods=["POST"])
+def quanlydonhang_Chobanxacnhan():
+    conn = get_connect()
+    cursor = conn.cursor()
+    sql = """
+    SELECT *
+    FROM QLBanQuanAo.DonHang
+    Where TrangThai Like 'Chờ bạn xác nhận'
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('adminorder.html', rows=rows)
+
+qldonhangbp.route("/quanlydonhang_Dagiaothanhcong", methods=["POST"])
+def quanlydonhang_Dagiaothanhcong():
+    conn = get_connect()
+    cursor = conn.cursor()
+    sql = """
+    SELECT *
+    FROM QLBanQuanAo.DonHang
+    Where TrangThai Like 'Đã giao thành công'
+    """
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('adminorder.html', rows=rows)
+
+qldonhangbp.route("/setTrangThai", methods=["POST", "GET"])
+
 def setTrangThai():
     conn = get_connect()
     cursor = conn.cursor()
